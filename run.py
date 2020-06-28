1d3df50d3622d31a02e89b41f9a518eb4e823f23
Matias Valdenegro <matias.valdenegro@gmail.com>
2020-06-29 00:51:21 +0200

    Also set the application icon.

--- run.py
+++ run.py
@@ -1,12 +1,21 @@
+#!/bin/python3
+
 import sys
 
 from PySide2.QtWidgets import QApplication
+from PySide2.QtGui import QIcon, QPixmap
 from wikinotebook import MainWindow
+
+import pathlib, os
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
     app.setApplicationName("wikinotebook")
+
+    path = os.path.join(pathlib.Path(__file__).parent.absolute(), "wikinotebook/icons/notes.svg")
+    icon = QIcon(QPixmap(path))
+    app.setWindowIcon(icon)
 
     mw = MainWindow()
     mw.show()
